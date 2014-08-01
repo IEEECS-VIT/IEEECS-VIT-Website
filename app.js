9c80cd52148cfe129c833a5971df209bb4d6fe5b
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-08-01 18:38:09 +0530

    Updated Dependencies
    Updated Dependencies in package.json
    UPdated app.js to use new packages
    Project is now ready for development
    Added favicon from http://computer.org/

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -10,16 +10,21 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use(logger('dev'));
+
+app.set('title', 'IEEE Computer Society-VIT Student Chapter');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var cookieSecret = process.env.COOKIE_SECRET || 'randomsecretstring';
+app.use(cookieParser(cookieSecret, {signed: true}));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -31,10 +36,8 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
+// error handlers
+// development error handler, will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
@@ -45,8 +48,7 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// production error handler, no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -54,6 +56,5 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
