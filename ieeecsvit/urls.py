c43a3838a9f817aab653c688b62958f52f922e72
Kunal Nagpal <kunagpal@gmail.com>
2015-03-20 19:30:06 +0530

    Adjusted home routes.

--- ieeecsvit/urls.py
+++ ieeecsvit/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+import internal.views
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'ieeecsvit.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
-
+                       url('/', internal.views.my_view.as_view(), name='index',),
                        url(r'^admin/', include(admin.site.urls)),
-)
+                       )
